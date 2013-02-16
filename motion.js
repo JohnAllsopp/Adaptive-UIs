346d01cc38170cdcc0b13b7981fdd24c94deabc0
John Allsopp <john@westciv.com>
2013-02-16 11:30:45 +1100

    made state changes sticky
    you can specify how long after the state (shaking or not) changes
    before making the sttyle change

--- motion.js
+++ motion.js
@@ -5,10 +5,16 @@
 var shakometer = {
 	//this manages the shakometer stuff
 	
-	motionInterval: 5000, 
+	motionInterval: 500, 
 	//set the interval in ms that you want to check for shaking over 
 	//devicemotion events fire hundreds of times a second, and provcessing each one will chew battery life
 	lastMotionEvent: 0, // as yet we've had none, keeps track of when we had the last motion event
+	
+	remainInState: 10000,
+	//when a change in state is detected (from shaking to non shaking or the other way)
+	//how long before we change the state of the UI in ms
+	
+	lastStateChange: 0, // time the state was last changed from shaking to non-shaking or the other way 
 	
 	start: function(){
 		//start the shakometer
@@ -52,20 +58,30 @@ var shakometer = {
 
 			if (shakometer.wasShaken(motionData.acceleration.x, motionData.acceleration.y, motionData.acceleration.z)){
 				
-				if (document.body.classList) {
-					document.body.classList.add("shaken")
-					document.body.classList.remove("regular")
+				//we know the device is shaking, but do we change the state yet?
+				//check how long since we changed the state, and if this exceeds shakometer.remainInState then change it
+				
+				if((today.getTime() - shakometer.lastStateChange) > shakometer.remainInState){	
+					
+					shakometer.lastStateChange = today.getTime()
+					//record when this state change ocurred
+				
+					if (document.body.classList) {
+						document.body.classList.add("shaken")
+						document.body.classList.remove("regular")
+					}
+					//we change the class name of the containing body, and so our shaken styles apply
+			
+					else {
+						if (document.body.classList) {
+							document.body.classList.add("regular")
+							document.body.classList.remove("shaken")
+						}
+						//we change the class name of the containing form, and so our regular styles apply	
+					}
 				}
-				//we change the class name of the containing body, and so our shaken styles apply
 			}
 			
-			else {
-				if (document.body.classList) {
-					document.body.classList.add("regular")
-					document.body.classList.remove("shaken")
-				}
-				//we change the class name of the containing form, and so our regular styles apply	
-			}
 
 			console.log(shakometer.wasShaken(motionData.acceleration.x, motionData.acceleration.y, motionData.acceleration.z) + " " + (today.getTime() - shakometer.lastMotionEvent))
 			//for debugging
